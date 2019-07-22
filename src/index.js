83a6a8e38bde938a0db3327276e5f523f273ca7d
Jowita <jowita.lapies@gmail.com>
2019-07-22 08:50:25 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
+const allToysDiv = document.querySelector("#toy-collection")
 let addToy = false
 
+const baseUrl = "http://localhost:3000"
+const toysUrl = baseUrl + "/toys"
 // YOUR CODE HERE
 
 addBtn.addEventListener('click', () => {
@@ -16,4 +19,88 @@ addBtn.addEventListener('click', () => {
 })
 
 
+
+
 // OR HERE!
+
+const  formSubmitHandler = event => {
+  event.preventDefault()
+  const toyObj = {
+    name: event.target.name.value,
+    image: event.target.image.value,
+    likes: 0,
+  }
+
+  fetch(toysUrl, {
+    method: "POST",
+    headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+    },
+    body: JSON.stringify(toyObj)
+  })
+  .then( resp=>resp.json())
+  .then( renderToy )
+}
+
+toyForm.addEventListener("submit", formSubmitHandler )
+
+const handleLikeClick = toy => {
+  fetch(toysUrl + `/${toy.id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      likes: toy.likes + 1
+    })
+  })
+  .then( fetchAndRenderAllToys )
+}
+const renderToy = toy => {
+  const newCardElement = document.createElement("div")
+  newCardElement.className = "card"
+
+  const newHeader =  document.createElement("h2")
+  newHeader.innerHTML = toy.name 
+
+  const newImage = document.createElement("img")
+  newImage.src = toy.image 
+  newImage.className = "toy-avatar"
+
+  const newLikes = document.createElement("p")
+  newLikes.innerHTML = `${toy.likes} Likes`
+
+  const newLikeBtn = document.createElement("button")
+  newLikeBtn.innerText = "Like <3"
+  newLikeBtn.className = "like-btn"
+  newLikeBtn.addEventListener( "click", () => handleLikeClick(toy) )
+
+  // const HTMLEls = [headerEl, imgEl, pEl, btnEl]
+  // HTMLEls.forEach( el => newCard.appendChild(el) )
+
+  newCardElement.appendChild( newHeader )
+  newCardElement.appendChild( newImage )
+  newCardElement.appendChild( newLikes )
+  newCardElement.appendChild( newLikeBtn )
+
+  allToysDiv.appendChild( newCardElement )
+}
+
+
+
+const renderAllToys = (toys) => {
+  allToysDiv.innerHTML = ""
+  toys.forEach(renderToy)
+}
+
+const fetchAllToys = () => {
+  return fetch(toysUrl)
+  .then(resp => resp.json())
+}
+
+const fetchAndRenderAllToys = () => {
+  fetchAllToys().then( renderAllToys )
+}
+
+fetchAndRenderAllToys()
